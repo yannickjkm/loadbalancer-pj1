ee1d68f76487235eefc2d2344d2aacede0eee3c8
yannick_km <yannick_km@outlookcom>
2021-04-14 21:36:39 +0100

    commit 1

---
+++ main.tf
@@ -0,0 +1,26 @@
+provider "aws" {
+    region  = "eu-west-1"
+    profile = "sawa"
+}
+
+// Calling the network module
+
+module "network_module" {
+    source = "./network_module"
+}
+
+
+// calling the loadbalancer module
+
+module "loadbalancer_module" {
+    source       = "./loadbalancer_module"
+    public_sg_id = module.network_module.public_sg_id
+}
+
+
+// Calling the autoscaling group module
+
+module "autoscaling_module" {
+    source        = "./autoscaling_module"
+    private_sg_id = module.network_module.private_sg_id
+}
